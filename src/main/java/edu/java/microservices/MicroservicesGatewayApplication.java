a41035d8038cb4ef574bad7430bd1bab7f8f62b9
Suslov-DS <https://github.com/solek57/student-project>
2021-01-20 18:35:03 +0400

    microservices

---
+++ src/main/java/edu/java/microservices/MicroservicesGatewayApplication.java
@@ -0,0 +1,11 @@
+package edu.java.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesGatewayApplication.class, args);
+    }
+}
---
+++ src/main/java/edu/java/microservices/web/EndpointController.java
@@ -0,0 +1,33 @@
+package edu.java.microservices.web;
+
+import edu.java.microservices.adapter.BackendAdapter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequiredArgsConstructor
+@Slf4j
+public class EndpointController {
+    @Autowired
+    private  BackendAdapter backendAdapter;
+
+    @Value("${instance.id}")
+    private int instanceId;
+
+    @Value("${secret}")
+    private String secret;
+
+
+    @GetMapping("/")
+    public String getRequestsCount() {
+        String result = String.format("Number of requests %s (gateway %d, secret %s)",
+                backendAdapter.getRequests(), instanceId, secret);
+        log.info("Handling count request. Result {}", result);
+        return result;
+    }
+
+}
